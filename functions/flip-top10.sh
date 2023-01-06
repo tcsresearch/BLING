f5a246e82f6cea46e75f817d992da04c439906d4
tcsresearch <tcsresearch@users.noreply.github.com>
2023-01-06 06:34:00 -0600

    Add FLIPTool functions

---
+++ functions/flip-top10.sh
@@ -0,0 +1,3 @@
+echo "Top 10 Largest Installed Packages:"
+echo "All sizes are shown in megabytes (MB)."
+rpm -qa --queryformat '%10{size} - %-25{name} \t %{version} \t %{os} \n' | sort -rh | head -25 | awk '{print $1/1024/1024, $2, $3, $4}'
