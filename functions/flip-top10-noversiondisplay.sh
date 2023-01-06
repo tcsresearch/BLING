6916231e2460469166b6db77c12740e567cd0d33
tcsresearch <tcsresearch@users.noreply.github.com>
2023-01-06 06:39:29 -0600

    Update and rename flip-top10-noversiondisplay.sh to flip-top10-noversiondisplay.func

--- functions/flip-top10-noversiondisplay.sh
+++
@@ -1,5 +0,0 @@
-echo "Top 10 Largest Installed Packages:"
-echo "All sizes are shown in megabytes (MB)."
-
-rpm -qa --queryformat '%10{size} - %-25{name} \t  %{os} \n' | sort -rh | head -25 | awk '{print $1/1024/1024, $2, $3}'
-
