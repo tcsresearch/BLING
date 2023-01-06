04c0317da94384763b66e402c6e49f0b4856b046
tcsresearch <tcsresearch@users.noreply.github.com>
2023-01-06 06:43:29 -0600

    Update flip-top10.sh

--- functions/flip-top10.sh
+++ functions/flip-top10.sh
@@ -1,3 +1,7 @@
+flip-top10() {
+  echo "FLIPTool - Find Largest Installed Packages (FLIP) Tool"
+  echo "Listing Type: Top 10"
+  echo "Version Display: Yes"
 echo "Top 10 Largest Installed Packages:"
 echo "All sizes are shown in megabytes (MB)."
 rpm -qa --queryformat '%10{size} - %-25{name} \t %{version} \t %{os} \n' | sort -rh | head -25 | awk '{print $1/1024/1024, $2, $3, $4}'
