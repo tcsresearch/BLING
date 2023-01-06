9c8f64296dcf8a6b902ce5748442ec316e70bda8
tcsresearch <tcsresearch@users.noreply.github.com>
2023-01-06 06:31:58 -0600

    Add FLIPTool-ng

---
+++ FLIPTool-ng-20230106-1/FLIPTool-ng/_Testing/flip-all2.sh
@@ -0,0 +1,25 @@
+## TODO: Fix queryformartargs - not working
+
+corecmd="rpm -qa"
+queryformatargs="--queryformat '%10{size} - %-25{name} \t %{version}\n' "
+queryformatargs2=" '%10{size} - %-25{name} \t %{version}\n'"
+qfargs3=" '%10{size} - %-25{name} \t' "
+
+# The queryformatargs are broken down individually here:
+querysize=" %10{size}"
+queryname="%-25{name}"
+queryversion="%{version}"
+
+sortargs=" -rh" # initial options: -n / -rh
+
+### $corecmd $queryformatargs | sort $sortargs
+
+### FIXME: The following line fails to display the package size (displays as 0):
+# $corecmd --queryformat '%10{size} - %-25{name} \t %{version}\n' | sort $sortargs
+
+### Test
+$corecmd --queryformat '%10{size} - %-25{name} \t %{version}\n'  | sort $sortargs
+
+### rpm -qa --queryformat '%10{size} - %-25{name} \t %{version}\n' | sort -n
+### rpm -qa --queryformat '%10{size} - %-25{name} \t %{version}\n' | sort -rh
+
---
+++ FLIPTool-ng-20230106-1/FLIPTool-ng/_Testing/flip-top10_v2.sh
@@ -0,0 +1,9 @@
+echo "Top 10 Largest Installed Packages:"
+echo "All sizes are shown in megabytes (MB)."
+###
+### Define Variables ###
+MainCMD="rpm -qa"
+SortArgs="-rh"
+HeadArgs="-25"
+#
+rpm -qa --queryformat '%10{size} - %-25{name} \t %{version} \t %{os} \n' | sort -rh | head -25 | awk '{print $1/1024/1024, $2, $3, $4}'
