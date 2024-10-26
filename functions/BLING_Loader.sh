ce54f5f31d403ef383cae6dbf1f828bb1bd2b62b
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2024-10-26 06:43:33 -0500

    Update BLING_Loader.sh

--- functions/BLING_Loader.sh
+++ functions/BLING_Loader.sh
@@ -1,5 +1,25 @@
-# Assuming 'files.list' contains a list of filenames, one per line
-for file in $(cat BLING_functions.list); do
-  echo "Loading Function: $file"
-  source $file
-done
+function BasicLoader() {
+  # Assuming 'files.list' contains a list of filenames, one per line
+  for file in $(cat BLING_functions.list); do
+    echo "Loading Function: $file"
+    source $file
+  done
+  
+}
+
+function AdvancedLoader() {
+  # Assuming 'files.list' contains a list of filenames, one per line
+  for file in $(cat BLING_functions.list); do
+    echo "Loading Function: $file"
+    source $file
+    if [ $? -eq 0 ] 
+    then 
+      echo "Successfully Loaded Function: $file." 
+    exit 0 
+    else 
+      echo "ERROR: Could not load function: $file! " >&2 
+    exit 1 
+    fi
+  done
+}
+
