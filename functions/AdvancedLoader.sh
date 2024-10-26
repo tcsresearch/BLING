5f551dd5791bcc6e8e2d0eaf53477bac530b973a
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2024-10-26 07:23:07 -0500

    Update AdvancedLoader.sh

--- functions/AdvancedLoader.sh
+++ functions/AdvancedLoader.sh
@@ -1,14 +1,14 @@
 function AdvancedLoader() {
 ### FIXME: Running script causes logout with or without a parameter.
 # If no parameters are specified, show help screen.
-if [ -z "$1" ]; then
-  echo "Usage: $0 <filename>"
-  echo "Where <filename> is the name of the list you wish to load."
+# if [ -z "$1" ]; then
+ # echo "Usage: $0 <filename>"
+ # echo "Where <filename> is the name of the list you wish to load."
   # exit 1 # Could be cause of logout.
-fi
+# fi
 
   # Assuming 'files.list' contains a list of filenames, one per line
-  for file in $(cat $1); do
+  for file in $(cat functions.list); do
     echo "Loading Function: $file"
     source $file
     if [ $? -eq 0 ] 
