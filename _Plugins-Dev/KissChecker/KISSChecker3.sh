a73d0dd475b047d514eb6a48b2028d5e000f03d6
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2024-10-26 03:30:01 -0500

    Update KISSChecker3.sh

--- _Plugins-Dev/KissChecker/KISSChecker3.sh
+++ _Plugins-Dev/KissChecker/KISSChecker3.sh
@@ -1,3 +1,18 @@
+# This script will run various static analyser checks on the Linux kernel, as per the Makefile.
+# Requires Kernel Makefile and compiled sources and/or vmlinux file.
+#
+### Core Commands- no logging ###
+# make checkstack
+# make versioncheck
+# make includecheck
+# make export_report
+# make headerdep
+# make coccicheck
+# make clang-analyzer
+# make clang-tidy
+
+#############################################################################################
+
 ### Run Our Functions
 DisplayBanner
 SanityCheck_LogDirExists
