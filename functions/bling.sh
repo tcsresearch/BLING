aa5118b7fc7ae41c7c2e2f9b160baff73ae4b108
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2025-12-13 19:46:35 -0600

    Create bling.sh

---
+++ functions/bling.sh
@@ -0,0 +1,20 @@
+# bling.sh - BLING Main Utility
+
+#/bin/bash
+
+function BLING_DisplayBanner() {
+  echo "BLING - BASH Library Next Gen - Version $BLING_Version."
+  echo " "
+}
+
+function BLING_ListFunctions() {
+  BLING_DisplayBanner
+  echo "This will display a list of all BLING functions."
+  echo "Not Implemented Yet!"
+}
+
+function BLING_Help() {
+  BLING_DisplayBanner
+  echo "This will display help for the specified function."
+  echo "Not Implemented Yet!"
+}
