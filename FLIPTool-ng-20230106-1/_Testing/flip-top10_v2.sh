56202299ab5eec146378d56c6be1063c6bb7ecef
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-16 17:50:30 -0500

    Refactor flip-top10_v2.sh to use variables
    Added variables for command arguments and improved readability.

--- FLIPTool-ng-20230106-1/_Testing/flip-top10_v2.sh
+++ FLIPTool-ng-20230106-1/_Testing/flip-top10_v2.sh
@@ -1,10 +1,18 @@
+#!/usr/bin/env bash
 # TODO: Use variables.
 echo "Top 10 Largest Installed Packages:"
 echo "All sizes are shown in megabytes (MB)."
 ###
 ### Define Variables ###
 MainCMD="rpm -qa"
+
 SortArgs="-rh"
-HeadArgs="-25"
+HeadArgs="-25" $Number Of Packages
+NumberOfPackages="25"
+AwkArgs="print $1/1024/1024, $2, $3, $4"
 #
+# This one should work.
 rpm -qa --queryformat '%10{size} - %-25{name} \t %{version} \t %{os} \n' | sort -rh | head -25 | awk '{print $1/1024/1024, $2, $3, $4}'
+
+# Testing 05/16/2026.
+$MainCMD --queryformat '%10{size} - %-25{name} \t %{version} \t %{os} \n' | sort -rh | head "$HeadArgs" | awk '{"$AwkArgs"}'
