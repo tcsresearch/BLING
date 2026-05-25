41f9f29fb7af1e59aa1389eb4ada3be22fbc7fa6
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-24 22:52:18 -0500

    Add files via upload

---
+++ FuncUtil.sh
@@ -0,0 +1,47 @@
+# FuncUtil.sh
+#  This script helps debug comples scripts that use many functions.
+#
+#  TODO: Have INI-based functions display in a seperate section.
+#        Use Cecho-like ability from VieweINI.sh
+
+# Store results inside a variable.
+NumberOfFoundFunctions=$(eval cat *.bfunc | grep function | grep { | wc -l)
+
+function ShowFunctions() {
+	## TODO: Use cut to remove 'function' and '() {' from displaying
+	cat *.bfunc | grep function | grep {
+	echo " "
+}
+
+function GetNumberOfFunctions() {
+	echo "Number Of Functions Found: $NumberOfFoundFunctions. "
+	# cat *.bfunc | grep function | grep { | wc -l
+}
+
+function IsFunctionDefined() {
+	## TODO: Have ShowFunctions output generate a list of functions for this to use.
+	if [ "$(type -t my_function)" == "function" ]; then
+ 	  echo "Function my_function is defined."
+	else
+  	  echo "Function my_function is NOT defined."
+	fi
+}
+
+function AreFunctionDefined() {
+        ## TODO: Have ShowFunctions output generate a list of functions for this to use.
+        my_function_list=`cat functions.list`
+	if [ "$(type -t my_function_list)" == "function" ]; then
+          echo "Function my_function is defined."
+        else
+          echo "Function my_function is NOT defined."
+        fi
+}
+
+
+# Run Our Function
+ShowFunctions
+GetNumberOfFunctions
+
+# IsFunctionDefined
+#
+
