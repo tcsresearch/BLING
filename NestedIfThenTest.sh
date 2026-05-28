2bbf00f7ab3bc67bdc0f7d2a3b79cb7850a006ae
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-28 02:53:47 -0500

    Add files via upload

---
+++ NestedIfThenTest.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+   
+DIRNAME="test_dir"
+FILENAME="test_file"
+  
+if [ ! -d "$DIRNAME" ]
+then
+  mkdir $DIRNAME
+  echo "Directory $DIRNAME created"
+  if [ ! -f "$FILENAME" ]
+  then
+    touch $DIRNAME/$FILENAME
+    echo "File $DIRNAME/$FILENAME created"
+  else
+    echo "File $DIRNAME/$FILENAME exists"
+  fi
+else
+  echo "Directory $DIRNAME exists"
+fi 
