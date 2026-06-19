2987908c2079031fb6fc7d651b66557b6e32b2c3
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-06-18 21:53:49 -0500

    Implement config and function sourcing in KISSChecker
    Added functions to source configuration and function files.

--- _Plugins-Dev/KissChecker/KISSChecker.sh
+++ _Plugins-Dev/KissChecker/KISSChecker.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
+# Kernel Indepth Static Sanity (KISS) Checker Tool 
 # This script will run various static analyser checks on the Linux kernel, as per the Makefile.
 # Requires Kernel Makefile and compiled sources and/or vmlinux file.
 #
@@ -14,10 +15,48 @@
 # make clang-tidy
 
 #############################################################################################
+# Define Config & Function Files #                                                          #
+#############################################################################################
 
-### Run Our Functions
+KCT_ConfigFile="KISSChecker.conf"
+KCT_FuncFile="KISSChecker.bfunc"
+
+#############################################################################################
+# Source Config #                                                                           #
+#############################################################################################
+
+function KCT_SourceConfig(){
+  if [ ! -f "$KCT_ConfigFile" ]; then
+    echo "ERROR: File $KCT_ConfigFile Not Found!"
+    return
+  else
+    source "$KCT_ConfigFile"
+  fi
+}
+
+function KCT_SourceFunctions(){
+  if [ ! -f "$KCT_FuncFile" ]; then
+    echo "ERROR: File $KCT_FuncFile Not Found!"
+    return
+  else
+    source "$KCT_FuncFile"
+  fi
+}
+
+
+#############################################################################################
+# Main Program #                                                                            #
+#############################################################################################
+
+### Source Config & Functions Library ###
+KCT_SourceConfig
+KCT_SourceFunctions
+
+### Run Startup Functions ###
 DisplayBanner
 SanityCheck_LogDirExists
 SanityCheck_MakeFileExists
 # SanityCheck_IsKernelMakefile
+
+### Run Main Program ###
 Main_Program
