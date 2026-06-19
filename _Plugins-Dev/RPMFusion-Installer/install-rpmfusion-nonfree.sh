a289c334b48bd2c01bffacdc2bd59197c9b9a603
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-06-18 22:06:59 -0500

    Refactor RPM Fusion installer script for Fedora 44
    Updated the script to define variables for Fedora version and base URLs for RPM Fusion packages.

--- _Plugins-Dev/RPMFusion-Installer/install-rpmfusion-nonfree.sh
+++ _Plugins-Dev/RPMFusion-Installer/install-rpmfusion-nonfree.sh
@@ -1,1 +1,19 @@
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-35.noarch.rpm
+#!/bin/env bash
+
+## Define Variables ##
+Prog="dnf"
+FedoraReleaseVer="44"
+
+BaseURL_Free="https://download1.rpmfusion.org/free/fedora"
+BaseURL_NonFree="https://download1.rpmfusion.org/nonfree/fedora"
+URL="https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-44.noarch.rpm"
+Package_BaseName_Free="rpmfusion-free-release"
+Package_BaseName_NonFree="rpmfusion-nonfree-release"
+
+# dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-44.noarch.rpm
+
+### Main Program ##
+## TODO: Perform 'rpm -q rpmfusion-free-release' and 'rpmfusion-nonfree-release'
+$prog install $BaseURL_Free/$Package_BaseName_Free-$FedoraReleaseVer.noarch.rpm
+$prog install $BaseURL_NonFree/$Package_BaseName_NonFree-$FedoraReleaseVer.noarch.rpm
+
