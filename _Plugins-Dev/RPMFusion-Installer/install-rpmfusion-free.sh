8bce4114953a53ef7d018fb0053c182b0c5ab831
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-06-18 22:13:48 -0500

    Update RPM Fusion repository URL to version 44

--- _Plugins-Dev/RPMFusion-Installer/install-rpmfusion-free.sh
+++ _Plugins-Dev/RPMFusion-Installer/install-rpmfusion-free.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-35.noarch.rpm
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-44.noarch.rpm
